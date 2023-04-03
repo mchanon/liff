f9443258e0d5e3bdcea7fe7dc18e3ca7b2bf0f3f
Chanon S <chanon@live.com>
2023-04-03 20:51:28 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -2,22 +2,22 @@ const desiredUserId = "xxx";
 const currentUserId = "xxx"; // Replace this with the actual user ID from your system
 
 const cart = [
-    { product: "แป้งพร้อมกล่อง 4\"", price: 15.00, quantity: 0 },
-    { product: "แป้งพร้อมกล่อง 5\"", price: 17.00, quantity: 0 },
-    { product: "แป้งพร้อมกล่อง 7\"", price: 20.00, quantity: 0 },
-    { product: "แป้งพร้อมกล่อง 9\"", price: 30.00, quantity: 0 },
-    { product: "แป้งพร้อมกล่อง 12\"", price: 45.00, quantity: 0 },
-    { product: "แป้งเปล่า 4\"", price: 12.00, quantity: 0 },
-    { product: "แป้งเปล่า 5\"", price: 14.00, quantity: 0 },
-    { product: "แป้งเปล่า 7\"", price: 17.00, quantity: 0 },
-    { product: "แป้งเปล่า 9\"", price: 25.00, quantity: 0 },
-    { product: "แป้งเปล่า 12\"", price: 35.00, quantity: 0 },
-    { product: "ซอสพิซซ่า", price: 110.00, quantity: 0 },
-    { product: "ซอสมายอง", price: 85.00, quantity: 0 },
-    { product: "ซอสเทาซัน", price: 140.00, quantity: 0 },
-    { product: "ซอสสไปซี่", price: 140.00, quantity: 0 },
-    { product: "ชีส 2.3kg", price: 700.00, quantity: 0 },
-    { product: "ชีส 2.5kg", price: 750.00, quantity: 0 },
+    { product: "แป้งพร้อมกล่อง 4\"", price: 15, quantity: 0 },
+    { product: "แป้งพร้อมกล่อง 5\"", price: 17, quantity: 0 },
+    { product: "แป้งพร้อมกล่อง 7\"", price: 20, quantity: 0 },
+    { product: "แป้งพร้อมกล่อง 9\"", price: 30, quantity: 0 },
+    { product: "แป้งพร้อมกล่อง 12\"", price: 45, quantity: 0 },
+    { product: "แป้งเปล่า 4\"", price: 12, quantity: 0 },
+    { product: "แป้งเปล่า 5\"", price: 14, quantity: 0 },
+    { product: "แป้งเปล่า 7\"", price: 17, quantity: 0 },
+    { product: "แป้งเปล่า 9\"", price: 25, quantity: 0 },
+    { product: "แป้งเปล่า 12\"", price: 35, quantity: 0 },
+    { product: "ซอสพิซซ่า", price: 110, quantity: 0 },
+    { product: "ซอสมายอง", price: 85, quantity: 0 },
+    { product: "ซอสเทาซัน", price: 140, quantity: 0 },
+    { product: "ซอสสไปซี่", price: 140, quantity: 0 },
+    { product: "ชีส 2.3kg", price: 700, quantity: 0 },
+    { product: "ชีส 2.5kg", price: 750, quantity: 0 },
 ];
 
 function showCheckoutForm() {
@@ -44,7 +44,7 @@ function displayCartItems() {
         const subtotalCell = document.createElement("td");
 
         productCell.textContent = item.product;
-        priceCell.textContent = "฿" + item.price.toFixed(2);
+        priceCell.textContent = "฿" + item.price.toFixed(0);
 
         // Quantity input and buttons
         const decreaseButton = document.createElement("button");
@@ -70,7 +70,7 @@ function displayCartItems() {
 
         // Calculate subtotal and update the total
         const subtotal = item.price * item.quantity;
-        subtotalCell.textContent = "฿" + subtotal.toFixed(2);
+        subtotalCell.textContent = "฿" + subtotal.toFixed(0);
         total += subtotal;
 
         row.appendChild(productCell);
@@ -80,7 +80,7 @@ function displayCartItems() {
         cartItems.appendChild(row);
     });
 
-    totalPrice.textContent = total.toFixed(2);
+    totalPrice.textContent = total.toFixed(0);
 }
 
 function updateQuantity(index, delta) {
